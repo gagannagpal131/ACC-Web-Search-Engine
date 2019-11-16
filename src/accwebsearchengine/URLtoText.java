35040741f984120a5b6c7d54cf0557a0faff7131
gagannagpal131 <gagannagpal131@gmail.com>
2019-11-15 19:15:07 -0500

    Website URLs converted to txt files in the “URL” folder. Text Processing and JSoup integrated in the code.

--- src/accwebsearchengine/URLtoText.java
+++ src/accwebsearchengine/URLtoText.java
@@ -1,10 +1,48 @@
 package accwebsearchengine;
 
+import java.io.*;
+import java.util.ArrayList;
+import org.jsoup.Jsoup;
+import textprocessing.*;
+
 public class URLtoText {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		ArrayList<String> urlList = new ArrayList<>();
+		
+		//Use URL of own PC while running on local computer
+		In in = new In("/Users/gagandeepnagpal/Desktop/ACC-Web-Search-Engine/src/accwebsearchengine/websites.txt");
+		
+        while (!in.isEmpty()) {
+        	
+        	String myText = in.readLine();
+        	//System.out.println(myText);
+        	urlList.add(myText);
+        	    	
+        }
+        
+        
+        for(int i = 0; i < urlList.size(); i++) {
+        	
+        	try {
+        		
+        		org.jsoup.nodes.Document doc = Jsoup.connect(urlList.get(i)).get();
+        		String text = doc.text();
+            	String FilePath = "/Users/gagandeepnagpal/Desktop/ACC-Web-Search-Engine/src/accwebsearchengine/urls/" + (i)+".txt" ;
+            	PrintWriter out = new PrintWriter(FilePath);
+        		out.println(urlList.get(i));
+            	out.println(text);
+        		System.out.println(urlList.get(i));
+        		out.close();
+        		
+        		
+        	}catch(Exception e){
+        		
+        		System.out.println("Exception, Cannot be converted to text: "+ urlList.get(i));
+        	}
+        	
+        
+        }
 	}
-
 }
